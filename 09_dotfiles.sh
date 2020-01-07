55b385a56eb794dc5f10fc348ccb69de6712e1af
xerifeazeitona <juliano.amaral@gmail.com>
2020-01-06 22:09:08 -0500

    fix typo

--- 09_dotfiles.sh
+++ 09_dotfiles.sh
@@ -8,7 +8,7 @@ echo Downloading and installing config files...
 cd /tmp
 dir=$(mktemp -d)
 [ ! -d "/home/$1" ] && mkdir -p "/home/$1" && chown -R "$1:wheel" "/home/$1"
-chown -R "$name:wheel" "$dir"
+chown -R "$1:wheel" "$dir"
 sudo -u "$1" git clone https://github.com/xerifeazeitona/dotfiles.git "$dir/gitrepo" >/dev/null 2>&1 &&
 sudo -u "$1" cp -rfT "$dir/gitrepo" "/home/$1"
 rm -f "/home/$1/README.md" "/home/$1/LICENSE"
